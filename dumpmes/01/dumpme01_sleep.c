d500a7144f5278ba7e3ca95a9a27d01285823318
juliusdeane <48096490+juliusdeane@users.noreply.github.com>
2021-05-17 12:33:09 +0200

    Add files via upload
    dumpme01_sleep source code.

---
+++ dumpmes/01/dumpme01_sleep.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main() {
+	const char *secret1 = "This is a very secret secret.";
+	const char *secret2 = "There's no business like show business.";
+	const char *secret3 = "There's no business like computer business.";
+
+	while(1) {
+		sleep(30);
+	}
+	return 0;
+}
