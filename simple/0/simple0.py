ad147c34dccb8aeef0a2267f63ef275f541352cf
juliusdeane <48096490+juliusdeane@users.noreply.github.com>
2021-06-15 09:20:58 +0200

    Simply0.py missing in the repository.

---
+++ simple/0/simple0.py
@@ -0,0 +1,16 @@
+import frida
+
+def on_message(message, data):
+    print("[on_message] message:", message, "data:", data)
+
+session = frida.attach("simple0")
+
+script = session.create_script("""
+rpc.exports.enumerateModules = function () {
+  return Process.enumerateModules();
+};
+""")
+script.on("message", on_message)
+script.load()
+
+print([m["name"] for m in script.exports.enumerate_modules()])
