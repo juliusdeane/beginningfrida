4ad6d432c13560e881b0104cd9ca09dd3d942319
juliusdeane <48096490+juliusdeane@users.noreply.github.com>
2021-06-15 09:28:43 +0200

    Create simple4.c

---
+++ simple/4/simple4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+const char* string_to_match = "AAAABBBBAAAA";
+
+int main() {
+    printf("Doing nothing (infinite bucle):\n");
+
+    while(1) {
+	    string_to_match;
+    }
+
+    return 0;
+}
